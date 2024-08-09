6d98a69824ab2bf58ee93eb6358645b37a19d00f
Roman Pozharskiy <rnpozharskiy@gmail.com>
2024-08-09 16:04:14 +0300

    fixes

--- usvm-util/src/main/kotlin/org/usvm/collections/immutable/implementations/immutableMap/TrieNode.kt
+++ usvm-util/src/main/kotlin/org/usvm/collections/immutable/implementations/immutableMap/TrieNode.kt
@@ -873,7 +873,7 @@ class TrieNode<K, V>(
         }
     }
 
-    fun keys() = UPersistentHashMapKeysIterator(this).asSequence().toSet()
+    fun keys() = UPersistentHashMapKeysIterator(this) as Iterator<K>
 
     fun isEmpty() = firstOrNull() == null
     fun isNotEmpty() = firstOrNull() != null
@@ -918,7 +918,7 @@ class TrieNode<K, V>(
         return result
     }
 
-    fun toMutableMap(): MutableMap<in K, V> = mutableMapOf<K, V>().also {
+    fun toMutableMap(): MutableMap<K, V> = mutableMapOf<K, V>().also {
         this.forEach {entry -> it[entry.key] = entry.value } }
 
     @Suppress("UNCHECKED_CAST")
