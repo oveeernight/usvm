be802ef6d6e24e1dc3f84fd058835c402f5e40bc
Roman Pozharskiy <rnpozharskiy@gmail.com>
2024-12-13 20:53:34 +0300

    move serialized params to primary ctor

--- usvm-net/src/test/kotlin/executor/IlTestExecutorDecoderApi.kt
+++ usvm-net/src/test/kotlin/executor/IlTestExecutorDecoderApi.kt
@@ -10,35 +10,69 @@ class IlTestExecutorDecoderApi(val ctx: IlContext): DecoderApi<IlTestExpr> {
 
     fun arrangeStmts(): List<IlTestStmt> = arrangeStmts
 
-    override fun createBoolConst(value: Boolean): IlTestExpr = IlTestConst.BoolConst(value, ctx.boolType)
-    override fun createCharConst(value: Char): IlTestExpr = IlTestConst.CharConst(value, ctx.charType)
-    override fun createInt8Const(value: Byte): IlTestExpr = IlTestConst.Int8Const(value, ctx.int8Type)
-    override fun createInt16Const(value: Short): IlTestExpr = IlTestConst.Int16Const(value, ctx.int16Type)
-    override fun createInt32Const(value: Int): IlTestExpr = IlTestConst.Int32Const(value, ctx.int32Type)
-    override fun createInt64Const(value: Long): IlTestExpr = IlTestConst.Int64Const(value, ctx.int64Type)
-    override fun createUInt8Const(value: UByte): IlTestExpr = IlTestConst.UInt8Const(value, ctx.uint8Type)
-    override fun createUInt16Const(value: UShort): IlTestExpr = IlTestConst.UInt16Const(value, ctx.uint16Type)
-    override fun createUInt32Const(value: UInt): IlTestExpr = IlTestConst.UInt32Const(value, ctx.uint32Type)
-    override fun createUInt64Const(value: ULong): IlTestExpr = IlTestConst.UInt64Const(value, ctx.uint64Type)
-    override fun createFloatConst(value: Float): IlTestExpr = IlTestConst.FloatConst(value, ctx.floatType)
-    override fun createDoubleConst(value: Double): IlTestExpr = IlTestConst.DoubleConst(value, ctx.doubleType)
-    override fun createStringConst(value: String): IlTestExpr = IlTestConst.StringConst(value, ctx.doubleType)
-    override fun createNullConst(type: IlType): IlTestExpr = IlTestConst.NullConst(type)
-    override fun createArray(elementType: IlType, size: Int, address: Int): IlTestExpr =
-        ArrayInstance(elementType, size, address)
+    override fun createBoolConst(value: Boolean): IlTestExpr =
+        IlTestConst.BoolConst(StmtKind.BOOL, value, ctx.boolType.toTypeRepr())
+    override fun createCharConst(value: Char): IlTestExpr =
+        IlTestConst.CharConst(StmtKind.CHAR, value, ctx.charType.toTypeRepr())
+    override fun createInt8Const(value: Byte): IlTestExpr =
+        IlTestConst.Int8Const(StmtKind.INT8, value, ctx.int8Type.toTypeRepr())
+    override fun createInt16Const(value: Short): IlTestExpr =
+        IlTestConst.Int16Const(StmtKind.INT16, value, ctx.int16Type.toTypeRepr())
+    override fun createInt32Const(value: Int): IlTestExpr =
+        IlTestConst.Int32Const(StmtKind.INT32, value, ctx.int32Type.toTypeRepr())
+    override fun createInt64Const(value: Long): IlTestExpr =
+        IlTestConst.Int64Const(StmtKind.INT64, value, ctx.int64Type.toTypeRepr())
+    override fun createUInt8Const(value: UByte): IlTestExpr =
+        IlTestConst.UInt8Const(StmtKind.UINT8, value, ctx.uint8Type.toTypeRepr())
+    override fun createUInt16Const(value: UShort): IlTestExpr =
+        IlTestConst.UInt16Const(StmtKind.UINT16, value, ctx.uint16Type.toTypeRepr())
+    override fun createUInt32Const(value: UInt): IlTestExpr =
+        IlTestConst.UInt32Const(StmtKind.UINT32, value, ctx.uint32Type.toTypeRepr())
+    override fun createUInt64Const(value: ULong): IlTestExpr =
+        IlTestConst.UInt64Const(StmtKind.UINT64, value, ctx.uint64Type.toTypeRepr())
+    override fun createFloatConst(value: Float): IlTestExpr =
+        IlTestConst.FloatConst(StmtKind.FLOAT, value, ctx.floatType.toTypeRepr())
+    override fun createDoubleConst(value: Double): IlTestExpr =
+        IlTestConst.DoubleConst(StmtKind.DOUBLE, value, ctx.doubleType.toTypeRepr())
+    override fun createStringConst(value: String): IlTestExpr =
+        IlTestConst.StringConst(StmtKind.STRING, value, ctx.stringType.toTypeRepr())
+    override fun createNullConst(type: IlType): IlTestExpr =
+        IlTestConst.NullConst(StmtKind.NULL, type.toTypeRepr())
+    override fun createArray(type: IlType, size: Int, address: Int): IlTestExpr =
+        ArrayInstance(StmtKind.NEW_ARRAY, type.toTypeRepr(), size, address)
     override fun createObject(type: IlType, address: Int): IlTestExpr =
-        ObjectInstance(type, address)
+        ObjectInstance(StmtKind.NEW_OBJ, type.toTypeRepr(), address)
 
+    override fun createCyclicReference(type: IlType, address: Int): IlTestExpr =
+        CyclicReference(StmtKind.CYCLIC_REFERENCE, type.toTypeRepr(), address)
+
+    // TODO ctor call
     override fun callMethod(method: IlMethod, args: List<IlTestExpr>): IlTestExpr {
-        return if (method.isStatic) IlTestCall.StaticMethodCall(method, args)
-        else IlTestCall.InstanceMethodCall(args[0], method, args.drop(1)).also { arrangeStmts.add(it) }
+        val returnType = method.returnType.toTypeRepr()
+        return if (method.isStatic) {
+            IlTestCall.StaticMethodCall(StmtKind.STATIC_CALL, method.toMethodRepr(), returnType, args)
+        }
+        else {
+            IlTestCall.InstanceMethodCall(
+                StmtKind.INSTANCE_CALL,
+                method.toMethodRepr(),
+                returnType,
+                args[0],
+                args.drop(1)
+            ).also { arrangeStmts.add(it) }
+        }
     }
 
     override fun setObjectField(obj: IlTestExpr, field: IlField, value: IlTestExpr) {
-        arrangeStmts += ArrangeStmt.SetObjectField(obj, field, value)
+        arrangeStmts += ArrangeStmt.SetObjectField(StmtKind.SET_OBJ_FIELD, obj, field, value)
     }
 
     override fun setArrayIndex(array: IlTestExpr, index: Int, value: IlTestExpr) {
-        arrangeStmts += ArrangeStmt.SetArrayIndex(array, index, value)
+        arrangeStmts += ArrangeStmt.SetArrayIndex(StmtKind.SET_ARRAY_INDEX, array, index, value)
     }
 }
+
+private fun IlType.toTypeRepr(): TypeRepr =
+    TypeRepr(asmName, moduleToken, typeToken, genericArgs.map { it.toTypeRepr() })
+
+private fun IlMethod.toMethodRepr(): MethodRepr = MethodRepr(declaringType.toTypeRepr(), signature, name)
