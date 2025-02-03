660720eb432b426c0afcf943054a0d92c25e0fbe
Roman Pozharskiy <rnpozharskiy@gmail.com>
2025-02-04 02:02:04 +0300

    rebase

--- usvm-net/build.gradle.kts
+++ usvm-net/build.gradle.kts
@@ -2,20 +2,20 @@ import com.google.protobuf.gradle.id
 
 plugins {
     id("usvm.kotlin-conventions")
-    kotlin("plugin.serialization") version "1.9.20"
+    kotlin("plugin.serialization") version "2.1.10"
     id("com.google.protobuf") version "0.9.4"
 }
 
-sourceSets {
-    main {
-        java {
-            srcDirs += File("/home/rnpozharskiy/work/usvm/usvm-net/src/test/kotlin/executor/generated/java")
-        }
-        kotlin {
-            srcDirs += File("/home/rnpozharskiy/work/usvm/usvm-net/src/test/kotlin/executor/generated/kotlin")
-        }
-    }
-}
+//sourceSets {
+//    main {
+//        java {
+//            srcDirs += File("/home/rnpozharskiy/work/usvm/usvm-net/src/test/kotlin/executor/generated/java")
+//        }
+//        kotlin {
+//            srcDirs += File("/home/rnpozharskiy/work/usvm/usvm-net/src/test/kotlin/executor/generated/kotlin")
+//        }
+//    }
+//}
 
 dependencies {
     implementation(project(":usvm-core"))
@@ -28,16 +28,16 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
 
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.0")
 
 //    implementation("com.google.protobuf:protobuf-java:4.28.2")
 //    implementation("com.google.protobuf:protobuf-kotlin:4.28.2")
 
 
     testImplementation(kotlin("test"))
-    testImplementation("com.google.protobuf:protobuf-java:4.28.2")
-    testImplementation("com.google.protobuf:protobuf-kotlin:4.28.2")
-    implementation("com.github.petrukhinandrew:jacodb:e84e4996aaf68619df2f3aaa0820e7fa66a2cded")
+    testImplementation("com.google.protobuf:protobuf-java:4.29.0")
+    testImplementation("com.google.protobuf:protobuf-kotlin:4.29.0")
+    implementation("com.github.petrukhinandrew:jacodb:75dec37e320da87c31a17ed013bd350d57b9b615")
 }
 
 //protobuf {
