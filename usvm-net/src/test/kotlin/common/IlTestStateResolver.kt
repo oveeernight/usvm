6f3663eea88fe337be5e805798c24cb9239327b6
Roman Pozharskiy <rnpozharskiy@gmail.com>
2025-02-05 02:46:44 +0300

    grpc init

--- usvm-net/src/test/kotlin/common/IlTestStateResolver.kt
+++ usvm-net/src/test/kotlin/common/IlTestStateResolver.kt
@@ -138,13 +138,14 @@ abstract class IlTestStateResolver<T>(
         val length = memory.read(lengthKey).tryInt32() ?: error("array $evaledRef length is not integer")
         val array = decoderApi.createArray(elemType, length, evaledRef.address)
 
+        cache[evaledRef.address] = array
+
         for (index in 0 until length) {
             val indexKey = UArrayIndexLValue(sort, heapRef, ctx.mkBv(index), descriptor)
             val resolved = resolve(memory.read(indexKey), elemType)
             decoderApi.setArrayIndex(array, index, resolved)
         }
 
-        cache[evaledRef.address] = array
         return array
     }
 
---
+++ usvm-net/src/test/kotlin/executor/IlTestExecutor.kt
@@ -0,0 +1,55 @@
+package executor
+
+import com.google.protobuf.Message
+import common.IlTestStateResolver
+import org.jacodb.api.net.ilinstances.IlMethod
+import org.jacodb.api.net.ilinstances.IlType
+import org.usvm.machine.IlContext
+import org.usvm.machine.interpreter.IlMethodResult
+import org.usvm.machine.logger
+import org.usvm.machine.state.IlState
+import org.usvm.memory.UReadOnlyMemory
+import org.usvm.model.UModelBase
+import testrunner.expressions.*
+
+class IlTestExecutor(private val state: IlState, private val method: IlMethod) {
+    private val concreteRunner = ConcreteTestRunner(timeoutSec = 5)
+    fun execute() {
+        val model = state.models.first()
+        val memory = state.memory
+        val scope = MemoryScope(state.ctx, method, state.methodResult, model, memory)
+        val test = scope.createTest()
+
+        logger.error  {"Test serialized: ${test}" }
+        concreteRunner.run(test)
+    }
+
+    private class MemoryScope(
+        ctx: IlContext,
+        method: IlMethod,
+        result: IlMethodResult,
+        model: UModelBase<IlType>,
+        stateMemory: UReadOnlyMemory<IlType>
+    ) : IlTestStateResolver<Message>(ctx, method, result, model, stateMemory) {
+        override val decoderApi: IlTestExecutorDecoderApi = IlTestExecutorDecoderApi(ctx)
+
+        fun createTest(): TestExpressions.IlTest {
+            val args = resolveArgs()
+            val arrange = decoderApi.arrangeStmts().map { com.google.protobuf.Any.pack(it) }
+            val methodCall = decoderApi.callMethod(method, args)
+            val resultAsMessage = when (result) {
+                is IlMethodResult.Success -> result.result
+                is IlMethodResult.Exception -> result.exception
+                else -> TODO()
+            }.let { resolve(it, method.returnType) }
+            val test = ilTest {
+                arrangeStmts.addAll(arrange)
+                call = com.google.protobuf.Any.pack(methodCall)
+                expectedResult = com.google.protobuf.Any.pack(resultAsMessage)
+            }
+            return test
+        }
+    }
+
+
+}
