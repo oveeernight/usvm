5f7894b82d9300a90115c1629368587033509610
Roman Pozharskiy <rnpozharskiy@gmail.com>
2025-02-15 02:56:32 +0300

    rewrite executor process config

--- usvm-net/src/test/kotlin/executor/ConcreteTestRunner.kt
+++ usvm-net/src/test/kotlin/executor/ConcreteTestRunner.kt
@@ -2,47 +2,36 @@ package executor
 
 import IlMethodTestRunner
 import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.usvm.machine.logger
 import testrunner.expressions.ConcreteExecutorGrpcKt
 import testrunner.expressions.TestExpressions
+import java.io.Closeable
 import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.concurrent.TimeUnit
 
-class ConcreteTestRunner() {
-    fun run(test: TestExpressions.IlTest) : TestExpressions.ExecutionResult {
-        val executorPath = IlMethodTestRunner.executorPath
-        val executorDir = File(executorPath)
-        val samplesAsm = IlMethodTestRunner.samplesAsmPath;
+class ConcreteTestRunner(val proc: Process) {
 
-        val proc = ProcessBuilder().command("./TestExecutor.Application", "--asm", samplesAsm)
-            .directory(executorDir)
-            .start()
+    fun run(testBatch: TestExpressions.IlTestBatch) : TestExpressions.ExecutionResult {
         val port = 8980
         val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build()
         val stub = ConcreteExecutorGrpcKt.ConcreteExecutorCoroutineStub(channel)
+
         val result = runBlocking {
-            stub.execute(test)
+            delay(1000)
+            stub.execute(testBatch)
         }
-        shutdownServer()
-        val error = proc.errorStream.bufferedReader().readText()
-        logger.error { "Internal error occured while concrete executing: $error" }
 
         val isSuccess = result.resultCase == TestExpressions.ExecutionResult.ResultCase.SUCCESS
+        logger.error { proc.errorStream.bufferedReader().readText() }
         require(isSuccess) {
             val failReason = result.fail.reason
             "Some executions failed:\n$failReason"
         }
-//
         return result
-    }
-
-    private fun shutdownServer() {
-        val url = URL("http://localhost:8980/shutdown")
-        val conn = url.openConnection() as HttpURLConnection
-        conn.requestMethod = "GET"
     }
 
 }
